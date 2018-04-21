38db66c1b79e9bb3f2677efaf63831d5e076aa64
Unknown <danrousseau@protonmail.com>
2018-04-21 14:57:30 +0200

    Basic Enemy Logic
    Basic enemy move towards and shooting logic

---
+++ Assets/Dnaiel/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Dnaiel.Scripts
+{
+    public class Enemy : MonoBehaviour
+    {
+
+        private NavMeshAgent _agent;
+        private Transform _target;
+        
+        [SerializeField] private float _speed;
+
+
+        private void Awake()
+        {
+            _agent = gameObject.GetComponent<NavMeshAgent>();
+            _agent.speed = _speed;
+        }
+
+        private void Update()
+        {
+            if(_agent == null) return;
+            
+            _agent.SetDestination(_target.position);
+        }
+    }
+}
