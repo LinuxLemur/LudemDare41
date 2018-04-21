3f867af15b1f658a6fd39e78d884c2d8630f492f
Unknown <justin.billson@gmail.com>
2018-04-21 13:58:01 +0200

    Texture for you :)

--- Assets/Justin/Scripts/playerMovement.cs
+++ Assets/Justin/Scripts/playerMovement.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 namespace Justin
 {
-public class playerMovement : MonoBehaviour 
-{
-	public GameObject player;
-	private int speed = 10;
-
-    private void Start() 
+	public class playerMovement : MonoBehaviour
 	{
-		player = this.gameObject;
+		private GameObject player;
+		[SerializeField]
+		private int speed = 10;
+
+		private void Start ()
+		{
+			player = this.gameObject;
+		}
+
+		void Update ()
+		{
+			move ();
+		}
+
+		public void move ()
+		{
+			Vector3 Movement = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+
+			player.transform.position += Movement * speed * Time.deltaTime;
+		}
+
 	}
-
-	void Update() 
-	{
-		move();
-	}
-
-	public void move()
-	{
-		Vector3 Movement = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-		player.transform.position += Movement * speed * Time.deltaTime;
-	}
-
 }
-}
-
