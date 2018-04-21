27d85ccaf0eddf3f326d2091727acc736cbbd83f
Unknown <justin.billson@gmail.com>
2018-04-21 13:27:14 +0200

    J's first commit
    test

---
+++ Assets/Justin/Scripts/playerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Justin
+{
+public class playerMovement : MonoBehaviour 
+{
+	public GameObject player;
+	private int speed = 10;
+
+    private void Start() 
+	{
+		player = this.gameObject;
+	}
+
+	void Update() 
+	{
+		move();
+	}
+
+	public void move()
+	{
+		Vector3 Movement = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+		player.transform.position += Movement * speed * Time.deltaTime;
+	}
+
+}
+}
+
