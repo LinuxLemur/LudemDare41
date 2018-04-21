a8423113b1a5ece1741e451df23849b6a0b20773
Unknown <justin.billson@gmail.com>
2018-04-21 17:14:55 +0200

    Player Logic

--- Assets/Justin/Scripts/playerMovement.cs
+++ Assets/Justin/Scripts/playerMovement.cs
@@ -18,13 +18,42 @@ namespace Justin
 		void Update ()
 		{
 			move ();
+			rotate ();
 		}
 
 		public void move ()
 		{
-			Vector3 Movement = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			Vector3 Movement = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+			player.transform.position += Movement * speed * Time.deltaTime;			
+		}
 
-			player.transform.position += Movement * speed * Time.deltaTime;
+		public void rotate ()
+		{
+			if (Input.GetKey (KeyCode.RightArrow))			
+				player.transform.rotation = Quaternion.Euler(0,90,0);
+			
+			if (Input.GetKey (KeyCode.LeftArrow))			
+				player.transform.rotation = Quaternion.Euler(0,-90,0);
+
+			if (Input.GetKey (KeyCode.UpArrow))			
+				player.transform.rotation = Quaternion.Euler(0,0,0);
+			
+			if (Input.GetKey (KeyCode.DownArrow))			
+				player.transform.rotation = Quaternion.Euler(0,180,0);
+
+			// ↓ 8 way aiming ↓
+
+			if (Input.GetKey (KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow))			
+				player.transform.rotation = Quaternion.Euler(0,-135,0);
+
+			if (Input.GetKey (KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow))			
+				player.transform.rotation = Quaternion.Euler(0,135,0);
+
+			if (Input.GetKey (KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))			
+				player.transform.rotation = Quaternion.Euler(0,-45,0);
+
+			if (Input.GetKey (KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow))			
+				player.transform.rotation = Quaternion.Euler(0,45,0);
 		}
 
 	}
--- Assets/Justin/Scripts/shooting.cs
+++ Assets/Justin/Scripts/shooting.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 
 public class shooting : MonoBehaviour
 {
+	[SerializeField] private int bulletSpeed = 10;
+	[SerializeField] private GameObject bullet;
+	[SerializeField] private GameObject bulletSpawn;
+	[SerializeField] private float shotCooldown;
+	[SerializeField] private float currentShotCooldown;
 
 	// Use this for initialization
-	void Start ()
+	private void Awake ()
 	{
-
+		currentShotCooldown = shotCooldown;
+		if (bulletSpawn == null)
+		{
+			Debug.LogError ("No fire point assigned");
+			return;
+		}
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
+		currentShotCooldown -= Time.deltaTime;
+		listenForInput ();
+	}
 
+	void listenForInput ()
+	{
+		if (currentShotCooldown > 0) return;
+		
+		if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.DownArrow))
+			shoot ();
+		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.RightArrow))
+			shoot ();
+	}
+
+	void shoot ()
+	{
+		currentShotCooldown = shotCooldown;
+		var spawnedBullet = Instantiate (bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+
+		var rbPlayer = gameObject.GetComponent<Rigidbody>();
+		var rbBullet = spawnedBullet.gameObject.GetComponent<Rigidbody> ();
+		rbBullet.AddForce (transform.forward * (bulletSpeed * 50));
 	}
 }
