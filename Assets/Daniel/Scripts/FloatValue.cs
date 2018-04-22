5cbf13b452cdf46d8e75b75323e1395dcffa15ac
Unknown <danrousseau@protonmail.com>
2018-04-22 12:31:22 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Daniel/Scripts/FloatValue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FloatValue", menuName = "FloatValue")]
+public class FloatValue : ScriptableObject
+{
+    [SerializeField] private float value;
+}
--- Assets/Daniel/Tests/CardGame/Scripts/TestPlayer.cs
+++ Assets/Daniel/Tests/CardGame/Scripts/TestPlayer.cs
@@ -6,17 +6,12 @@ public class TestPlayer : MonoBehaviour
 {
     [SerializeField] private PlayerSystem _playerstats;
 
-    [SerializeField] private float _movespeed;
 
-    [SerializeField] private float _damage;
-
-    [SerializeField] private float _attackDelay;
-
-    
     private void Update()
     {
-        _movespeed = _playerstats.MovementSpeed;
-        _damage = _playerstats.Damage;
-        _attackDelay = _playerstats.AttackSpeed;
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            _playerstats.Health -= 10;
+        }
     }
 }
