d684a2e856df5206039436a47aa48c7915335f0c
Unknown <justin.billson@gmail.com>
2018-04-22 12:30:26 +0200

    Added multi bullets for the player

--- Assets/Justin/Scripts/shooting.cs
+++ Assets/Justin/Scripts/shooting.cs
@@ -5,43 +5,51 @@ using UnityEngine;
 namespace Justin
 {
 	public class shooting : MonoBehaviour
-{
-	[SerializeField] private int bulletSpeed = 10;
-	[SerializeField] private GameObject bullet;
-	[SerializeField] private GameObject bulletSpawn;
-	[SerializeField] private float shotCooldown;
-	[SerializeField] private float currentShotCooldown;
-
-	private void Awake ()
 	{
-		currentShotCooldown = shotCooldown;
-		if (bulletSpawn == null)
+		[Header ("Shooting Properties")]
+		[SerializeField] private int bulletSpeed = 10;
+		[SerializeField] private float fireDelay = 1f;
+		private float currentShotCooldown;
+
+		[Header ("Drag & Drop")]
+		[SerializeField] private GameObject bullet;
+		[SerializeField] private GameObject[] bulletSpawns;
+
+		private void Awake ()
 		{
-			Debug.LogError ("No fire point assigned");
-			return;
+			currentShotCooldown = fireDelay;
+
+			if (bulletSpawns == null)
+			{
+				Debug.LogError ("No fire point assigned");
+				return;
+			}
+		}
+
+		void Update ()
+		{
+			currentShotCooldown -= Time.deltaTime;
+			listenForInput ();
+		}
+
+		void listenForInput ()
+		{
+			if (currentShotCooldown > 0) return;
+
+			if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.RightArrow))
+				shoot ();
+		}
+
+		void shoot ()
+		{
+			currentShotCooldown = fireDelay;
+			for (int i = 0; i < bulletSpawns.Length; i++)
+			{
+				var spawnedBullet = Instantiate (bullet, bulletSpawns[i].transform.position, bulletSpawns[i].transform.rotation);
+				var rbBullet = spawnedBullet.gameObject.GetComponent<Rigidbody> ();
+				rbBullet.AddForce (transform.forward * (bulletSpeed * 50));
+			}
+
 		}
 	}
-
-	void Update ()
-	{
-		currentShotCooldown -= Time.deltaTime;
-		listenForInput ();
-	}
-
-	void listenForInput ()
-	{
-		if (currentShotCooldown > 0) return;
-
-		if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.RightArrow))
-			shoot ();
-	}
-
-	void shoot ()
-	{
-		currentShotCooldown = shotCooldown;
-		var spawnedBullet = Instantiate (bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
-		var rbBullet = spawnedBullet.gameObject.GetComponent<Rigidbody> ();
-		rbBullet.AddForce (transform.forward * (bulletSpeed * 50));
-	}
-}
 }
