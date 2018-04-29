e254cc4ab7a2f69eed731bd85d5bfe495d35930b
Unknown <justin.billson@gmail.com>
2018-04-29 16:48:56 +0200

    Added Player Respawn on fall

---
+++ Assets/Justin/Scripts/RespawnOnEnter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnOnEnter : MonoBehaviour
+{
+	[SerializeField] private GameObject DesignatedObject;
+	[SerializeField] private int respawnHeight;
+	private string _name;
+
+	private void Awake ()
+	{
+		_name = DesignatedObject.tag;
+	}
+
+	private void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag (_name))
+		{
+			RespawnObject ();
+		}
+	}
+
+	private void RespawnObject ()
+	{
+		DesignatedObject.transform.position = new Vector3 (0, respawnHeight, 0);
+	}
+}
